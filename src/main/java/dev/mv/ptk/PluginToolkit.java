fb35e1485d9f20169bd060c1206e4319a6e8709b
mqxf <mqxf@mvteam.dev>
2024-09-01 15:26:23 +0200

    Initial commit

---
+++ src/main/java/dev/mv/ptk/PluginToolkit.java
@@ -0,0 +1,16 @@
+package dev.mv.ptk;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public abstract class MVPluginToolkit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
